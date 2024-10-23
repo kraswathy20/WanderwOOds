5929566f49a3143ac5479b4bd35a5fa60d446bb9
Aswathy k.r <aswathy19kr@gmail.com>
2024-10-23 13:41:30 +0530

    error stack view changed in production

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-if(process.env.NODE_ENV !== "production"){
-    require('dotenv').config()
-}
+// if(process.env.NODE_ENV !== "production"){
+//     require('dotenv').config()
+// }
+
+require('dotenv').config()
 
 // console.log(process.env.CLOUDINARY_SECRET);
 // console.log(process.env.CLOUDINARY_CLOUD_NAME);
